840f1f545e9cbc163bdc0ddc34fe3b8d5b22a3c4
Samaan Kamal <samaankamal72@gmail.com>
2024-05-11 20:57:47 +0300

    added Helper class to act as Request for the book containg all its data

---
+++ src/main/java/com/example/LibrarySystem/Helpers/BookHelper/BookRequest.java
@@ -0,0 +1,35 @@
+package com.example.LibrarySystem.Helpers.BookHelper;
+
+
+public class BookRequest {
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String isbn;
+    private String edition;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
