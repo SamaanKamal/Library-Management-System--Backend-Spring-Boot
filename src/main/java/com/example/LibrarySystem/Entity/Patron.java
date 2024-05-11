151b2298da63a9b5cacf721bce96289ff52e2eee
Samaan Kamal <samaankamal72@gmail.com>
2024-05-11 19:57:05 +0300

    added patron entity

---
+++ src/main/java/com/example/LibrarySystem/Entity/Patron.java
@@ -0,0 +1,48 @@
+package com.example.LibrarySystem.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "patron")
+public class Patron {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "patron_id")
+    private Integer patronId;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "contact_information")
+    private String contactInformation;
+
+    public Patron() {
+    }
+
+    public Patron(String name, String contactInformation) {
+        this.name = name;
+        this.contactInformation = contactInformation;
+    }
+
+    public Integer getPatronId() {
+        return patronId;
+    }
+
+    public void setPatronId(Integer patronId) {
+        this.patronId = patronId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+}
