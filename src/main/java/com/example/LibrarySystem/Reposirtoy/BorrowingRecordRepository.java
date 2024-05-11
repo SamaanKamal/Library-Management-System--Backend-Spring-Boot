a6b0be675bdfc6ac0a7d789c204dedc6cdc12dbd
Samaan Kamal <samaankamal72@gmail.com>
2024-05-12 00:00:58 +0300

    added two methods for borrowing record repository to check availability

--- src/main/java/com/example/LibrarySystem/Reposirtoy/BorrowingRecordRepository.java
+++ src/main/java/com/example/LibrarySystem/Reposirtoy/BorrowingRecordRepository.java
@@ -1,7 +1,14 @@
 package com.example.LibrarySystem.Reposirtoy;
 
+import com.example.LibrarySystem.Entity.Book;
 import com.example.LibrarySystem.Entity.BorrowingRecord;
+import com.example.LibrarySystem.Entity.Patron;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface BorrowingRecordRepository extends JpaRepository<BorrowingRecord,Integer> {
+    Optional<BorrowingRecord> findByBookAndPatron(Book book, Patron patron);
+    boolean existsByBookIdAndReturnedDateIsNull(Integer bookId);
 }
