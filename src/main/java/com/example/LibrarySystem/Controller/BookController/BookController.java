0a7d05bc6c051690de56e023c482b0d0c1fa2f70
Samaan Kamal <samaankamal72@gmail.com>
2024-05-12 16:29:46 +0300

    added Book Controller for the CRUD endpoints required

---
+++ src/main/java/com/example/LibrarySystem/Controller/BookController/BookController.java
@@ -0,0 +1,79 @@
+package com.example.LibrarySystem.Controller.BookController;
+
+import com.example.LibrarySystem.Entity.Book;
+import com.example.LibrarySystem.Helpers.BookHelper.BookRequest;
+import com.example.LibrarySystem.Helpers.BookHelper.BookResponse;
+import com.example.LibrarySystem.Service.BookService.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    private IBookService bookService;
+
+    @GetMapping
+    public ResponseEntity<BookResponse> fetchBooks(){
+        List<Book> books =bookService.getAllBooks();
+        if(books==null){
+            return ResponseEntity.notFound().build();
+        }
+        BookResponse bookResponse = new BookResponse(books);
+        return ResponseEntity.ok(bookResponse);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> fetchBook(@PathVariable("id") Integer id){
+        if(id==null){
+            return ResponseEntity.badRequest().build();
+        }
+        Book book = bookService.getBook(id);
+        if(book ==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(book);
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody BookRequest bookRequest){
+        if(bookRequest==null){
+            return ResponseEntity.badRequest().build();
+        }
+        Book book = bookService.createBook(bookRequest);
+        if(book!=null){
+            return ResponseEntity.status(HttpStatus.CREATED).body(book);
+        }
+        return  ResponseEntity.internalServerError().build();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable("id") Integer id,@RequestBody BookRequest bookRequest){
+        if(bookRequest==null||id ==null){
+            return ResponseEntity.badRequest().build();
+        }
+        Book book = bookService.updateBook(id,bookRequest);
+        if(book!=null){
+            return ResponseEntity.status(HttpStatus.OK).body(book);
+        }
+        return  ResponseEntity.internalServerError().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteBook(@PathVariable("id") Integer id){
+        if(id ==null){
+            return ResponseEntity.badRequest().build();
+        }
+        boolean isDeleted= bookService.deleteBook(id);
+        if(isDeleted){
+            return ResponseEntity.status(HttpStatus.OK).body("Book Deleted Successfully");
+        }
+        return ResponseEntity.internalServerError().build();
+    }
+
+
+
+}
