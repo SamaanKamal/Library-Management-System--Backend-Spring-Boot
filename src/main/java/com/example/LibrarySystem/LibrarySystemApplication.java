8c4e652440b73cec2bbb5363f2919def1f8ca87f
Samaan Kamal <samaankamal72@gmail.com>
2024-05-14 17:15:59 +0300

    added enable caching annotation to allow spring in memory caching

--- src/main/java/com/example/LibrarySystem/LibrarySystemApplication.java
+++ src/main/java/com/example/LibrarySystem/LibrarySystemApplication.java
@@ -2,10 +2,12 @@ package com.example.LibrarySystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:env.properties")
+@EnableCaching
 public class LibrarySystemApplication {
 
 	public static void main(String[] args) {
